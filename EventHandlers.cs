914882342579d63dce751096db17e9999c1bc468
Raging-Tornado <welsbyharley2@gmail.com>
2026-01-06 00:17:55 +0800

    EventHandlers.cs

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -1,3 +1,5 @@
+using Exiled.API.Enums;
+using Exiled.API.Extensions;
 using Exiled.Events.EventArgs.Server;
 using MEC;
 using PlayerRoles;
@@ -33,6 +35,10 @@ namespace CustomRespawnSystem
         {
             Timing.KillCoroutines(RespawnCoroutineHandle);
         }
+        private static List<Player> ShufflePlayers(IEnumerable<Player> players)
+        {
+            return players.OrderBy(_ => UnityEngine.Random.value).ToList();
+        }
         public static IEnumerator<float> RespawnLoopMtfPriority()
         {
             while (!Round.IsLobby)
@@ -45,51 +51,80 @@ namespace CustomRespawnSystem
                 if (ChosenWave >= ciPercent)
                 {
                     int SpawnCount = 0;
-                    foreach (Player player in Player.List.Where(x => x.Role.Team == Team.Dead))
+                    var deadPlayers = ShufflePlayers(Player.List.Where(x => x.Role.Team == Team.Dead));
+                    foreach (Player player in deadPlayers)
                     {
                         if (player is null)
                             continue;
 
-                        if (SpawnCount == 0)
+                        if (SpawnCount == 0 && SpawnCount < Plugin.Instance.Config.MaximumMtfRespawnAmount)
                         {
                             player.Role.Set(RoleTypeId.NtfCaptain);
                             SpawnCount++;
                         }
-                        else if (SpawnCount >= 1 && SpawnCount <= 3)
+                        else if (SpawnCount >= 1 && SpawnCount <= 3 && SpawnCount < Plugin.Instance.Config.MaximumMtfRespawnAmount)
                         {
                             player.Role.Set(RoleTypeId.NtfSergeant);
                             SpawnCount++;
                         }
-                        else if (SpawnCount < 15)
+                        else if (SpawnCount < Plugin.Instance.Config.MaximumMtfRespawnAmount)
                         {
                             player.Role.Set(RoleTypeId.NtfPrivate);
                             SpawnCount++;
                         }
                     }
-                }
-                else
-                {
-                    int SpawnCount = 0;
-                    foreach (Player player in Player.List.Where(x => x.Role.Team == Team.Dead))
+                    int SCPAmount = 0;
+                    foreach (Player player in Player.List)
                     {
                         if (player is null)
                             continue;
 
-                        if (SpawnCount == 0)
+                        if (player.IsScp)
+                        {
+                            ++SCPAmount;
+                        }
+                    }
+                    if (SCPAmount == 0)
+                    {
+                        LabApi.Features.Wrappers.Cassie.Message($"MTFUNIT EPSILON 11 DESIGNATED NINETAILEDFOX HASENTERED . ALLREMAINING . NOSCPSLEFT .", $"Mobile Task Force unit, Epsilon-11, designated, Nine-Tailed Fox, has entered the facility.<split>All remaining personnel are advised to proceed with standard evacuation protocols, until an MTF squad reaches your destination.<split>Substantial threat to safety remains within the facility -- exercise caution.");
+                    }
+                    if (SCPAmount == 1)
+                    {
+                        LabApi.Features.Wrappers.Cassie.Message($"MTFUNIT EPSILON 11 DESIGNATED NINETAILEDFOX HASENTERED . ALLREMAINING . AWAITINGRECONTAINMENT {SCPAmount} SCPSUBJECT .", $"Mobile Task Force unit, Epsilon-11, designated, Nine-Tailed Fox, has entered the facility.<split>All remaining personnel are advised to proceed with standard evacuation protocols, until an MTF squad reaches your destination.<split>Awaiting recontainment of: {SCPAmount} SCP subject.");
+                    }
+                    if (SCPAmount > 1)
+                    {
+                        LabApi.Features.Wrappers.Cassie.Message($"MTFUNIT EPSILON 11 DESIGNATED NINETAILEDFOX HASENTERED . ALLREMAINING . AWAITINGRECONTAINMENT {SCPAmount} SCPSUBJECTS .", $"Mobile Task Force unit, Epsilon-11, designated, Nine-Tailed Fox, has entered the facility.<split>All remaining personnel are advised to proceed with standard evacuation protocols, until an MTF squad reaches your destination.<split>Awaiting recontainment of: {SCPAmount} SCP subjects.");
+                    }
+                }
+                else
+                {
+                    int SpawnCount = 0;
+                    var deadPlayers = ShufflePlayers(Player.List.Where(x => x.Role.Team == Team.Dead));
+                    foreach (Player player in deadPlayers)
+                    {
+                        if (player is null)
+                            continue;
+
+                        if (SpawnCount == 0 && SpawnCount < Plugin.Instance.Config.MaximumCiRespawnAmount)
                         {
                             player.Role.Set(RoleTypeId.ChaosMarauder);
                             SpawnCount++;
                         }
-                        else if (SpawnCount >= 1 && SpawnCount <= 3)
+                        else if (SpawnCount >= 1 && SpawnCount <= 3 && SpawnCount < Plugin.Instance.Config.MaximumCiRespawnAmount)
                         {
                             player.Role.Set(RoleTypeId.ChaosRepressor);
                             SpawnCount++;
                         }
-                        else if (SpawnCount < 15)
+                        else if (SpawnCount < Plugin.Instance.Config.MaximumCiRespawnAmount)
                         {
                             player.Role.Set(RoleTypeId.ChaosRifleman);
                             SpawnCount++;
                         }
+                    }
+                    foreach (Player player in Player.List.Where(x => x.Role.Side == Side.ChaosInsurgency))
+                    {
+                        player.Broadcast(25, "Chaos Insurgency has entered the Facility", Broadcast.BroadcastFlags.Normal, false);
                     }
                 }
                 yield return Timing.WaitForOneFrame;
@@ -112,21 +147,25 @@ namespace CustomRespawnSystem
                         if (player is null)
                             continue;
 
-                        if (SpawnCount == 0)
+                        if (SpawnCount == 0 && SpawnCount < Plugin.Instance.Config.MaximumCiRespawnAmount)
                         {
                             player.Role.Set(RoleTypeId.ChaosMarauder);
                             SpawnCount++;
                         }
-                        else if (SpawnCount >= 1 && SpawnCount <= 3)
+                        else if (SpawnCount >= 1 && SpawnCount <= 3 && SpawnCount < Plugin.Instance.Config.MaximumCiRespawnAmount)
                         {
                             player.Role.Set(RoleTypeId.ChaosRepressor);
                             SpawnCount++;
                         }
-                        else if (SpawnCount < 15)
+                        else if (SpawnCount < Plugin.Instance.Config.MaximumCiRespawnAmount)
                         {
                             player.Role.Set(RoleTypeId.ChaosRifleman);
                             SpawnCount++;
                         }
+                    }
+                    foreach (Player player in Player.List.Where(x => x.Role.Side == Side.ChaosInsurgency))
+                    {
+                        player.Broadcast(25, "Chaos Insurgency has entered the Facility", Broadcast.BroadcastFlags.Normal, false);
                     }
                 }
                 else
@@ -137,21 +176,44 @@ namespace CustomRespawnSystem
                         if (player is null)
                             continue;
 
-                        if (SpawnCount == 0)
+                        if (SpawnCount == 0 && SpawnCount < Plugin.Instance.Config.MaximumMtfRespawnAmount)
                         {
                             player.Role.Set(RoleTypeId.NtfCaptain);
                             SpawnCount++;
                         }
-                        else if (SpawnCount >= 1 && SpawnCount <= 3)
+                        else if (SpawnCount >= 1 && SpawnCount <= 3 && SpawnCount < Plugin.Instance.Config.MaximumMtfRespawnAmount)
                         {
                             player.Role.Set(RoleTypeId.NtfSergeant);
                             SpawnCount++;
                         }
-                        else if (SpawnCount < 15)
+                        else if (SpawnCount < Plugin.Instance.Config.MaximumMtfRespawnAmount)
                         {
                             player.Role.Set(RoleTypeId.NtfPrivate);
                             SpawnCount++;
                         }
+                    }
+                    int SCPAmount = 0;
+                    foreach (Player player in Player.List)
+                    {
+                        if (player is null)
+                            continue;
+
+                        if (player.IsScp)
+                        {
+                            ++SCPAmount;
+                        }
+                    }
+                    if (SCPAmount == 0)
+                    {
+                        LabApi.Features.Wrappers.Cassie.Message($"MTFUNIT EPSILON 11 DESIGNATED NINETAILEDFOX HASENTERED . ALLREMAINING . NOSCPSLEFT .", $"Mobile Task Force unit, Epsilon-11, designated, Nine-Tailed Fox, has entered the facility.<split>All remaining personnel are advised to proceed with standard evacuation protocols, until an MTF squad reaches your destination.<split>Substantial threat to safety remains within the facility -- exercise caution.");
+                    }
+                    if (SCPAmount == 1)
+                    {
+                        LabApi.Features.Wrappers.Cassie.Message($"MTFUNIT EPSILON 11 DESIGNATED NINETAILEDFOX HASENTERED . ALLREMAINING . AWAITINGRECONTAINMENT {SCPAmount} SCPSUBJECT .", $"Mobile Task Force unit, Epsilon-11, designated, Nine-Tailed Fox, has entered the facility.<split>All remaining personnel are advised to proceed with standard evacuation protocols, until an MTF squad reaches your destination.<split>Awaiting recontainment of: {SCPAmount} SCP subject.");
+                    }
+                    if (SCPAmount > 1)
+                    {
+                        LabApi.Features.Wrappers.Cassie.Message($"MTFUNIT EPSILON 11 DESIGNATED NINETAILEDFOX HASENTERED . ALLREMAINING . AWAITINGRECONTAINMENT {SCPAmount} SCPSUBJECTS .", $"Mobile Task Force unit, Epsilon-11, designated, Nine-Tailed Fox, has entered the facility.<split>All remaining personnel are advised to proceed with standard evacuation protocols, until an MTF squad reaches your destination.<split>Awaiting recontainment of: {SCPAmount} SCP subjects.");
                     }
                 }
                 yield return Timing.WaitForOneFrame;
